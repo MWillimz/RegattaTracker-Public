a81520c6357d861e0b6db46d96e4b56439cba3e2
MWillimz <94216706+MWillimz@users.noreply.github.com>
2026-08-30 16:16:44 +0200

    refactor: use string resources in QRScannerScreen.kt for #64

--- app/src/main/java/de/williserv/regattaclient/QRScannerScreen.kt
+++ app/src/main/java/de/williserv/regattaclient/QRScannerScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.res.stringResource
 import androidx.core.content.ContextCompat
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.BinaryBitmap
@@ -87,7 +88,7 @@ fun QrScannerScreen(
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
-            Text("Back")
+            Text(stringResource(R.string.back))
         }
     }
 }
