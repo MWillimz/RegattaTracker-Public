bd45e0ffeb3b3157b91f2b9489383fc63848aa04
MWillimz <94216706+MWillimz@users.noreply.github.com>
2026-09-22 09:38:11 +0200

    Fix session screen formatting imports

--- app/src/main/java/de/williserv/regattaclient/SessionHistoryScreen.kt
+++ app/src/main/java/de/williserv/regattaclient/SessionHistoryScreen.kt
@@ -1,6 +1,6 @@
 package de.williserv.regattaclient
 
-import android.text.format.DateFormat
+import java.text.DateFormat
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
